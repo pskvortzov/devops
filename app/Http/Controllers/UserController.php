7abf61cf55e927765ca3b3da618dc660c299819a
gh-actions <pskvortzov@gmail.com>
2025-01-31 17:40:16 +0000

    Laravel Pint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,49 +10,56 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() {
+    public function index()
+    {
         return [1];
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {
+    public function create()
+    {
         return [2];
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         return [3];
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(User $user) {
+    public function show(User $user)
+    {
         return [4];
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user) {
+    public function edit(User $user)
+    {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user) {
+    public function update(Request $request, User $user)
+    {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user) {
+    public function destroy(User $user)
+    {
         //
     }
 }
