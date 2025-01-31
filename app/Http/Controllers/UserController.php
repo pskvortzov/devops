610368de4c843a9fd3496816b60a5fedcbef207b
Pavel Skvortsov <pskvortzov@gmail.com>
2025-01-31 19:54:55 +0700

    Test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,8 +24,7 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
