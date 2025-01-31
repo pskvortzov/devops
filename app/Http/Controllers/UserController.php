0f4937c173aa2339db48e251016e550c95f79961
Pavel Skvortsov <pskvortzov@gmail.com>
2025-01-31 19:36:06 +0700

    Test pint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,8 +17,7 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
