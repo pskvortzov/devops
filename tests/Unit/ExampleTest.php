921c71dd3144d96dbd3b7fdb97c384a86d86d132
Pavel Skvortsov <pskvortzov@gmail.com>
2025-02-03 20:03:13 +0700

    Add mysql service to Run Tests action

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Models\User;
+
 test('that true is true', function () {
     expect(true)->toBeTrue();
 });
 
 test('that false is false', function () {
     expect(false)->toBeFalse();
+});
+
+test('that db is working', function() {
+    User::factory(5)->create();
+
+    $users = User::all();
+
+    expect($users->count())->toEqual(5);
 });
