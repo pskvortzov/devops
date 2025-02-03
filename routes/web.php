520e343e458f3ac6d27a5c5e89690484ec704cd9
gh-actions <pskvortzov@gmail.com>
2025-02-03 12:39:48 +0000

    Run Laravel Pint

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     Redis::incr('test');
+
     return Redis::get('test');
 });
