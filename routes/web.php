9869fc01f7bec9c87c90c02a8eb269363d57399e
Pavel Skvortsov <pskvortzov@gmail.com>
2025-02-03 19:39:11 +0700

    Add Redis

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
-    return view('welcome');
+    Redis::incr('test');
+    return Redis::get('test');
 });
